939ccbf0386bc6b558b454bc1a8da4dd7210942e
Molasz <martiolasz@gmail.com>
2024-01-29 20:21:42 +0100

    /021 heheh

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: molasz-a <molasz-a@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/25 12:11:20 by molasz-a          #+#    #+#             */
-/*   Updated: 2024/01/29 16:51:55 by molasz-a         ###   ########.fr       */
+/*   Updated: 2024/01/29 20:10:39 by molasz-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,19 +23,17 @@ void	test(int fd)
 		free(s);
 }
 
-int	main(void)
+int	main(int argc, char **argv)
 {
-	int		test1;
-	int		test2;
-	int		test3;
-	int		test4;
-	int		test5;
+	int	fd;
 
-	test1 = open("test/normal.txt", O_RDONLY);
-	test2 = open("test/empty.txt", O_RDONLY);
-	test3 = open("test/onlynl.txt", O_RDONLY);
-	test4 = open("test/nonl.txt", O_RDONLY);
-	test5 = open("test/1char.txt", O_RDONLY);
-	test(test2);
+	if (argc > 1)
+		fd = open(argv[1], O_RDONLY);
+	else
+		fd = open("test/empty.txt", O_RDONLY);
+	test(fd);
+	test(fd);
+	test(fd);
+	test(fd);
 	return (0);
 }
